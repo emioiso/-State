d049c0038314b8c286b52f28d4af7f9d219a6b75
emioiso <jijjjiijiijijiji@gmail.com>
2023-10-10 11:38:18 +0900

    Initial Commit

---
+++ @State/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  @State
+//
+//  Created by emi oiso on 2023/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ @State/_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  _StateApp.swift
+//  @State
+//
+//  Created by emi oiso on 2023/10/10.
+//
+
+import SwiftUI
+
+@main
+struct _StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
